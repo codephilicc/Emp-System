46ec0891a778ccb847753092222a15ebe4f26225
Rahul <rahulkr.vk@gmail.com>
2023-09-26 23:33:36 +0530

    create db

--- src/main/java/com/prog/controller/EmpController.java
+++ src/main/java/com/prog/controller/EmpController.java
@@ -1,15 +1,22 @@
 package com.prog.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.prog.entity.Employee;
+import com.prog.service.EmpService;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class EmpController {
 
+	@Autowired
+	private EmpService service;
+	
 	@GetMapping("/")
 	public String home() {
 		return "index";
@@ -21,10 +28,12 @@ public class EmpController {
 	}
 	
 	@PostMapping("/register")
-	public String empRegister(@ModelAttribute Employee e) {
+	public String empRegister(@ModelAttribute Employee e,HttpSession session) {
 		
 			System.out.println(e);
-		return "add_emp"; 
+			service.addEmp(e);
+			session.setAttribute("msg","Employee Added Sucessfully.. ");
+		return "redirect:/"; 
 	}
 	
 }
