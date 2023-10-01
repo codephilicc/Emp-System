dac3de9fa295da10c67a065f0620a85a38a37928
Rahul <rahulkr.vk@gmail.com>
2023-10-01 12:32:29 +0530

    body

--- src/main/java/com/prog/controller/EmpController.java
+++ src/main/java/com/prog/controller/EmpController.java
@@ -1,14 +1,18 @@
 package com.prog.controller;
 
-import org.hibernate.mapping.List;
+
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import com.prog.entity.Employee;
 import com.prog.service.EmpService;
+
 
 import jakarta.servlet.http.HttpSession;
 
@@ -19,8 +23,10 @@ public class EmpController {
 	private EmpService service;
 
 	@GetMapping("/")
-	public String home() {
-
+	public String home(Model m) {
+		
+		List<Employee> emp = service.getAllEmp();
+		m.addAttribute("emp",emp);
 		return "index";
 	}
 
