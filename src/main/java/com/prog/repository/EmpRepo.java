5b5819fd664023a97b24fbb2b5fc53ce1f278180
RahulKumar <87577357+codephilicc@users.noreply.github.com>
2024-01-18 21:33:06 +0530

    Update EmpRepo.java

--- src/main/java/com/prog/repository/EmpRepo.java
+++ src/main/java/com/prog/repository/EmpRepo.java
@@ -3,6 +3,7 @@ package com.prog.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 import com.prog.entity.Employee;
 
 @Repository
